3a09683879b673d5751708b68619b63423f45634
Frédéric Guillot <fguillot@users.noreply.github.com>
2014-03-19 18:27:46 -0400

    Add new content scrapper rule for pseudo-sciences.org

---
+++ lib/PicoFeed/Rules/www.pseudo-sciences.org.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'test_url' => 'http://www.pseudo-sciences.org/spip.php?article2275',
+    'body' => array(
+        '//div[@id="art_main"]',
+    ),
+    'strip' => array(
+    ),
+);
