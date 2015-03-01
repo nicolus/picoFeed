da9c8df79c76b00e2e7c16ae12f768117164a838
Frederic Guillot <fred@miniflux.net>
2015-03-01 18:58:00 -0500

    Add grabber rule for developpez.com

---
+++ lib/PicoFeed/Rules/www.developpez.com.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+    'test_url' => 'http://www.developpez.com/actu/81757/Mozilla-annonce-la-disponibilite-de-Firefox-36-qui-passe-au-HTTP-2-et-permet-la-synchronisation-de-son-ecran-d-accueil/',
+    'body' => array(
+         '//div[@itemprop="articleBody"]',
+    ),
+    'strip' => array(
+    )
+);
