beac91bf7cda7057d470c495b6616ced44ef1936
Frederic Guillot <fred@miniflux.net>
2015-03-01 13:36:28 -0500

    Add test case for feed with whitespace content and not empty description

--- tests/Parser/Rss20ParserTest.php
+++ tests/Parser/Rss20ParserTest.php
@@ -203,6 +203,14 @@ class Rss20ParserTest extends PHPUnit_Framework_TestCase
 
     public function testItemContent()
     {
+        $parser = new Rss20(file_get_contents('tests/fixtures/allgemeine-zeitung.xml'));
+        $feed = $parser->execute();
+        $this->assertNotEmpty($feed->items);
+        $this->assertEquals(
+            '<p>OBER-FLÖRSHEIM - (red). Sein erheblich beschädigtes Fahrzeug hat ein Fahrer in der Nacht von Donnerstag auf Freitag mitten auf der Ober-Flörsheimer Hauptstraße stehen lassen. Beim Verlassen des Autos in Richtung ...</p>',
+            $feed->items[2]->getContent()
+        );
+
         $parser = new Rss20(file_get_contents('tests/fixtures/rss20.xml'));
         $feed = $parser->execute();
         $this->assertNotEmpty($feed->items);
