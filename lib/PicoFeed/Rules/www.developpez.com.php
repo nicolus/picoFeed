1803af807b3125a27c4cd0c7efbc9a1fe425ef6d
Mirado ZAKASOA <me.radoou@gmail.com>
2015-03-02 09:53:20 +0300

    Correction grabber developpez.com

--- lib/PicoFeed/Rules/www.developpez.com.php
+++ lib/PicoFeed/Rules/www.developpez.com.php
@@ -1,9 +1,21 @@
 <?php
 return array(
-    'test_url' => 'http://www.developpez.com/actu/81757/Mozilla-annonce-la-disponibilite-de-Firefox-36-qui-passe-au-HTTP-2-et-permet-la-synchronisation-de-son-ecran-d-accueil/',
-    'body' => array(
-         '//div[@itemprop="articleBody"]',
-    ),
-    'strip' => array(
-    )
+	 'test_url' => 'http://www.developpez.com/actu/81757/Mozilla-annonce-la-disponibilite-de-Firefox-36-qui-passe-au-HTTP-2-et-permet-la-synchronisation-de-son-ecran-d-accueil/',
+	 'body' => array(
+	     	'//div[@itemprop="articleBody"]',
+	 ),
+	'strip' => array(
+		'//script',
+		'//style',
+		'//form',
+		'//div[@class="content"]/img',
+		'//a[last()]/following-sibling::*',
+		'//*[contains(@class,"actuTitle")]',
+		'//*[contains(@class,"date")]',
+		'//*[contains(@class,"shareButtons")]',
+		'//*[contains(@class,"inlineimg")]',
+		'//*[@id="leftColumn"]',
+		'//*[@id="signaler"]',
+		'//*[@id="signalerFrame"]',
+	)
 );
