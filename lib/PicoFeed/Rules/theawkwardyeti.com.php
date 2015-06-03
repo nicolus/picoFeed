96d4b011029157de075e8e0c9744486938739a9b
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-06-03 11:30:35 +0700

    rules: switche theawkwardyeti to filtering

--- lib/PicoFeed/Rules/theawkwardyeti.com.php
+++ lib/PicoFeed/Rules/theawkwardyeti.com.php
@@ -1,12 +1,9 @@
 <?php
 return array(
-    'grabber' => array(
-        '%/comic/.*%' => array(
-            'test_url' => 'http://theawkwardyeti.com/comic/happy-friday-2/',
-            'body' => array(
-                '//div[@id="comic"]//img'
-            ),
-            'strip' => array()
+    'filter' => array(
+        '%.*%' => array(
+             '%(width|height)="\\d+"%' => "",
+	     '%-\\d+x\\d+%' => "",
         )
     )
 );
