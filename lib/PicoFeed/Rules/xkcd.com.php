26c9b466e026ffce85c8aa1b3a8592700e930b48
Hoàng Đức Hiếu <hdhoang@zahe.me>
2015-06-03 11:32:38 +0700

    rules: extract xkcd alt text

--- lib/PicoFeed/Rules/xkcd.com.php
+++ lib/PicoFeed/Rules/xkcd.com.php
@@ -1,13 +1,8 @@
 <?php
 return array(
-    'grabber' => array(
+    'filter' => array(
         '%.*%' => array(
-            'test_url' => 'http://xkcd.com/1472/',
-            'body' => array(
-                '//div[@id="comic"]',
-            ),
-            'strip' => array(
-            ),
+	    '%alt="(.+)" */>%' => "/><br/>$1"
         )
     )
 );
