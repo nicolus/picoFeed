1c05ac093e507265ed8111fa9da5931f946d313c
Mathias Kresin <m@kresin.me>
2015-07-08 21:41:48 +0200

    Move tests
    Both fixtures are used to check functionality that is implement in the parser
    class.

--- tests/Parser/ParserTest.php
+++ tests/Parser/ParserTest.php
@@ -44,4 +44,25 @@ class ParserTest extends PHPUnit_Framework_TestCase
         $feed = $parser->execute();
         $this->assertNotEmpty($feed->items);
     }
+
+    public function testFeedEncodingsAreSupported()
+    {
+        // windows-1251
+        $parser = new Rss20(file_get_contents('tests/fixtures/ibash.ru.xml'));
+        $feed = $parser->execute();
+        $this->assertEquals('<p>Хабр, обсуждение фейлов на работе: reaferon: Интернет-магазин с оборотом более 1 млн. в месяц. При округлении цены до двух знаков после запятой: $price = round($price,2); была допущена досадная опечатка $price = rand($price,2);</p>', $feed->items[0]->getContent());
+
+        // CP1251
+        $parser = new Rss20(file_get_contents('tests/fixtures/xakep.ru.xml'));
+        $feed = $parser->execute();
+        $this->assertEquals('Bug Bounty — другая сторона медали', $feed->items[23]->title);
+        $this->assertEquals('<p>Бывший директор АНБ, генерал Майкл Хэйден снова показал себя во всей красе.</p>', $feed->items[0]->getContent());
+    }
+
+    public function testXMLTagStrippingIsUsed()
+    {
+        $parser = new Rss20(file_get_contents('tests/fixtures/jeux-linux.fr.xml'));
+        $feed = $parser->execute();
+        $this->assertNotEmpty($feed->items);
+    }
 }
