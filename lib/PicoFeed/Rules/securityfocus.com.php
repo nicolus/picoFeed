5420e5198a1da43588c7e320129754c4878535bc
Colin W <git@part4.net>
2016-01-11 10:48:51 -0600

    small changes

--- lib/PicoFeed/Rules/securityfocus.com.php
+++ lib/PicoFeed/Rules/securityfocus.com.php
@@ -5,7 +5,6 @@ return array(
         '%.*%' => array(
             'test_url' => 'http://www.securityfocus.com/news/11569?ref=rss',
             'body' => '//div[@class="expanded"]',
-            'next_page' => '//v[@align="right"]/a',
         ),
     ),
 );
--- lib/PicoFeed/Rules/soundandvision.com.php
+++ lib/PicoFeed/Rules/soundandvision.com.php
@@ -5,7 +5,6 @@ return array(
         '%.*%' => array(
             'test_url' => 'http://www.soundandvision.com/content/james-guthrie-mixing-roger-waters-and-pink-floyd-51',
             'body' => '//div[@id="left"]',
-            'next_page' => '//a[contains(@title,"Next Page")]',
             'strip' => array(
                 '//div[@class="meta"]',
                 '//div[@class="ratingsbox"]',
