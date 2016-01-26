bebe54530941021aae91b2f6a155dff5802313bd
Nikita Chernyi <rakshazi@users.noreply.github.com>
2016-01-26 16:40:34 +0200

    https changed to http for vk.com

--- lib/PicoFeed/Filter/Attribute.php
+++ lib/PicoFeed/Filter/Attribute.php
@@ -129,7 +129,7 @@ class Attribute
         'https://player.vimeo.com',
         'http://www.dailymotion.com',
         'https://www.dailymotion.com',
-        'https://vk.com',
+        'http://vk.com',
     );
 
     /**
