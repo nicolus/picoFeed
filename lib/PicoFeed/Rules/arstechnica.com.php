c7d956fad4c078075659782849dbbda3e745206c
Colin W <git@part4.net>
2017-02-13 16:33:35 -0600

    adjust arstechnica

--- lib/PicoFeed/Rules/arstechnica.com.php
+++ lib/PicoFeed/Rules/arstechnica.com.php
@@ -5,7 +5,7 @@ return array(
         '%.*%' => array(
             'test_url' => 'http://arstechnica.com/tech-policy/2015/09/judge-warners-2m-happy-birthday-copyright-is-bogus/',
             'body' => array(
-                '//div[@class="left-column"]',
+                '//article',
             ),
             'strip' => array(
                 '//h4[@class="post-upperdek"]',
@@ -14,9 +14,11 @@ return array(
                 '//figcaption',
                 '//aside',
                 '//div[@class="gallery-image-credit"]',
-                '//div[@class="article-expander"]',
-                '//div[@class="column-wrapper"]'
+                '//section[@class="article-author"]',
+                '//*[contains(@id,"social-")]',
+                '//div[contains(@id,"footer")]'
             ),
         ),
     ),
 );
+
