55166028a541a67f60f21c8e8ee88a2c972b76f3
Colin W <git@part4.net>
2017-02-13 11:33:18 -0600

    update jsonline; add usatoday

--- lib/PicoFeed/Rules/usatoday.com.php
+++ lib/PicoFeed/Rules/usatoday.com.php
@@ -3,11 +3,12 @@
 return array(
     'grabber' => array(
         '%.*%' => array(
-            'test_url' => 'http://www.jsonline.com/news/usandworld/as-many-as-a-million-expected-for-popes-last-mass-in-us-b99585180z1-329688131.html',
+            'test_url' => 'http://www.usatoday.com/story/life/music/2017/02/13/things-you-should-know-happened-grammy-awards-2017/97833734/',
             'body' => array(
                 '//div[@itemprop="articleBody"]',
             ),
             'strip' => array(
+                '//script',
                 '//h1',
                 '//iframe',
                 '//span[@class="mycapture-small-btn mycapture-btn-with-text mycapture-expandable-photo-btn-small js-mycapture-btn-small"]',
@@ -15,13 +16,12 @@ return array(
                 '//div[contains(@class,"ui-video-wrapper")]',
                 '//div[contains(@class,"media-mob")]',
                 '//div[contains(@class,"left-mob")]',
-                '//p/strong',
                 '//div[contains(@class,"nerdbox")]',
                 '//div[contains(@class,"oembed-asset")]',
                 '//*[contains(@class,"share")]',
                 '//div[contains(@class,"gallery-asset")]',
                 '//div[contains(@class,"oembed-asset")]',
-                '//div[@class="article-print-url"]',
+                '//div[@class="article-print-url"]'
             ),
         ),
     ),
