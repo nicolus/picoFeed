52fbf7da453b74fa45b0765cbb1d74c0e18314f2
Colin W <cwmke@users.noreply.github.com>
2017-03-22 20:18:30 -0500

    Update Some Rules; Remove Extra White Space From Multiple Rules (#315)

--- lib/PicoFeed/Rules/csmonitor.com.php
+++ lib/PicoFeed/Rules/csmonitor.com.php
@@ -1,29 +1,18 @@
 <?php
-
 return array(
     'grabber' => array(
         '%.*%' => array(
             'test_url' => 'http://www.csmonitor.com/USA/Politics/2015/0925/John-Boehner-steps-down-Self-sacrificing-but-will-it-lead-to-better-government',
             'body' => array(
-            '//article',
+                '//h2[@id="summary"]',
+                '//div[@class="flex-video youtube"]',
+                '//div[contains(@class,"eza-body")]',
             ),
             'strip' => array(
-                '//h1',
                 '//span[@id="breadcrumb"]',
-                '//div[@id="head-tag"]',
-                '//div[@id="byline"]',
-                '//div[@id="right-column"]',
-                '//div[@class="injection"]',
-                '//div[@id="sticky-nav"]',
                 '//div[@id="byline-wrapper"]',
-                '//li[@class="video disabled"]',
-                '//div[@id="image-nav"]',
-                '//div[@class="header_group"]',
-                '//div[contains(@class,"caption_bar")]',
-                '//img[@title="hide caption"]',
+                '//div[@class="injection"]',
                 '//*[contains(@class,"promo_link")]',
-                '//div[@id="story-embed-column"]',
-                '//div[@class="story-foot"]'
             ),
         ),
     ),
