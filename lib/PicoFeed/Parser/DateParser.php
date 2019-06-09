9652438331582fc0904af9cae891109ddae4bfe8
nicolus <nbailly@previsite.com>
2019-06-09 14:49:54 +0200

    Ignore dates before Unix Epoch and try other formats This fixes RSS feeds using RFC822 dates (yy) instead of RSS dates (yyyy)

--- lib/PicoFeed/Parser/DateParser.php
+++ lib/PicoFeed/Parser/DateParser.php
@@ -74,7 +74,8 @@ class DateParser extends Base
             }
 
             $date = $this->getValidDate($format, $truncated_value);
-            if ($date !== false) {
+            // Ignore dates before 1970, it's likely not the right format
+            if ($date !== false && $date->getTimestamp() > 0) {
                 return $date;
             }
         }
